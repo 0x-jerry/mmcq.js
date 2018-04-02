a083df8902a24e91f8ea2b957d1ee0c74a84873f
cwxyz <jie844067636@gmail.com>
2018-04-02 20:48:27 +0800

    code refactoring

--- js/VBox.js
+++ js/VBox.js
@@ -5,65 +5,63 @@ function getColorIndex(r, g, b) {
     return (r << (2 * sigbits)) + (g << sigbits) + b;
 }
 
-// 3d color space box
-function VBox(r1, r2, g1, g2, b1, b2, histo) {
-  var vbox = this;
-  vbox.r1 = r1;
-  vbox.r2 = r2;
-  vbox.g1 = g1;
-  vbox.g2 = g2;
-  vbox.b1 = b1;
-  vbox.b2 = b2;
-  vbox.histo = histo;
-}
+export default class VBox {
+  constructor(r1, r2, g1, g2, b1, b2, histo) {
+    this.r1 = r1;
+    this.r2 = r2;
+    this.g1 = g1;
+    this.g2 = g2;
+    this.b1 = b1;
+    this.b2 = b2;
+    this.histo = histo;
+  }
 
-VBox.prototype = {
-  volume: function (force) {
-    var vbox = this;
-    if (!vbox._volume || force) {
-      vbox._volume = ((vbox.r2 - vbox.r1 + 1) * (vbox.g2 - vbox.g1 + 1) * (vbox.b2 - vbox.b1 + 1));
+  volume(force) {
+    if(!this._volume || force) {
+      this._volume = (this.r2 - this.r1 + 1) * (this.g2 -this.g1 + 1) * (this.b2 - this.b1 + 1)
     }
-    return vbox._volume;
-  },
-  count: function (force) {
-    var vbox = this,
-      histo = vbox.histo;
-    if (!vbox._count_set || force) {
-      var npix = 0,
-        index, i, j, k;
-      for (i = vbox.r1; i <= vbox.r2; i++) {
-        for (j = vbox.g1; j <= vbox.g2; j++) {
-          for (k = vbox.b1; k <= vbox.b2; k++) {
-            index = getColorIndex(i, j, k);
-            npix += (histo[index] || 0);
+    return this._volume
+  }
+
+  count(force) {
+    let index = 0, npix = 0
+
+    if(!this._count_set || force) {
+      for(let i = this.r1; i <= this.r2; i++) {
+        for(let j = this.g1; j <= this.g2; j++) {
+          for(let k = this.b1; k <= this.b2; k++) {
+            index = getColorIndex(i, j, k)
+            npix += this.histo[index] || 0
           }
         }
       }
-      vbox._count = npix;
-      vbox._count_set = true;
+      this._count = npix
+      this._count_set = true
     }
-    return vbox._count;
-  },
-  copy: function () {
-    var vbox = this;
-    return new VBox(vbox.r1, vbox.r2, vbox.g1, vbox.g2, vbox.b1, vbox.b2, vbox.histo);
-  },
-  avg: function (force) {
-    var vbox = this,
-      histo = vbox.histo;
-    if (!vbox._avg || force) {
-      var ntot = 0,
-        mult = 1 << (8 - sigbits),
+
+    return this._count
+  }
+
+  copy() {
+    return new VBox(this.r1, this.r2, this.g1, this.g2, this.b1, this.b2, this.histo)
+  }
+
+  avg(force) {
+    if (!this._avg || force) {
+      const mult = 1 << (8 - sigbits)
+
+      let ntot = 0,
+        hval = 0,
         rsum = 0,
         gsum = 0,
         bsum = 0,
-        hval,
-        i, j, k, histoindex;
-      for (i = vbox.r1; i <= vbox.r2; i++) {
-        for (j = vbox.g1; j <= vbox.g2; j++) {
-          for (k = vbox.b1; k <= vbox.b2; k++) {
+        histoindex;
+
+      for (let i = this.r1; i <= this.r2; i++) {
+        for (let j = this.g1; j <= this.g2; j++) {
+          for (let k = this.b1; k <= this.b2; k++) {
             histoindex = getColorIndex(i, j, k);
-            hval = histo[histoindex] || 0;
+            hval = this.histo[histoindex] || 0;
             ntot += hval;
             rsum += (hval * (i + 0.5) * mult);
             gsum += (hval * (j + 0.5) * mult);
@@ -71,25 +69,28 @@ VBox.prototype = {
           }
         }
       }
+
       if (ntot) {
-        vbox._avg = [~~(rsum / ntot), ~~(gsum / ntot), ~~(bsum / ntot)];
+        this._avg = [~~(rsum / ntot), ~~(gsum / ntot), ~~(bsum / ntot)];
       } else {
-        //  console.log('empty box');
-        vbox._avg = [~~(mult * (vbox.r1 + vbox.r2 + 1) / 2), ~~(mult * (vbox.g1 + vbox.g2 + 1) / 2), ~~(mult * (vbox.b1 + vbox.b2 + 1) / 2)];
+         console.log('empty box')
+
+        this._avg = [~~(mult * (this.r1 + this.r2 + 1) / 2),
+                     ~~(mult * (this.g1 + this.g2 + 1) / 2),
+                     ~~(mult * (this.b1 + this.b2 + 1) / 2)];
       }
     }
-    return vbox._avg;
-  },
-  contains: function (pixel) {
-    var vbox = this,
-      rval = pixel[0] >> rshift,
-      gval = pixel[1] >> rshift,
-      bval = pixel[2] >> rshift;
-    return (rval >= vbox.r1 && rval <= vbox.r2 &&
-      gval >= vbox.g1 && gval <= vbox.g2 &&
-      bval >= vbox.b1 && bval <= vbox.b2);
+
+    return this._avg;
   }
-};
 
+  contains(pixel) {
+    const rval = pixel[0] >> rshift
+    const gval = pixel[1] >> rshift
+    const bval = pixel[2] >> rshift
 
-module.exports = VBox
+    return rval >= this.r1 && rval <= this.r2 &&
+           gval >= this.g1 && gval <= this.g2 &&
+           bval >= this.b1 && bval <= this.b2
+  }
+}
