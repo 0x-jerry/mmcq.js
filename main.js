2e2158b1ebee9aedd2994fd7ca6972b3e2f6e4d1
cwxyz <jie844067636@gmail.com>
2018-05-21 10:47:43 +0800

    docs: update index.html

--- main.js
+++ main.js
@@ -1,28 +1,14 @@
-import $ from 'jquery'
-import ExtImgColor from './js/ExtImgColor'
+/*global extImgColor */
 
 $(window).ready(() => {
-  const extImgColor = new ExtImgColor()
-
   $('img').click((e) => {
-    extImgColor
-      .getPaletteAsync(e.currentTarget, 7, 6)
-      .then((colors) => {
-        $('.js-color').each((index, target) => {
-          if(index < colors.length){
-            $(target).css({
-              backgroundColor: `rgb(${colors[index].join(',')})`
-            })
-          }
+    const colors = extImgColor.getPalette(e.currentTarget, 7, 6)
+    $('.js-color').each((index, target) => {
+      if (index < colors.length) {
+        $(target).css({
+          backgroundColor: `rgb(${colors[index].join(',')})`
         })
-      })
-
-    extImgColor
-      .getColorAsync(e.currentTarget, 5)
-      .then((color) => {
-        $('.js-domainColor').css({
-          backgroundColor: `rgb(${color.join(',')})`
-        })
-      })
+      }
+    })
   })
 })
