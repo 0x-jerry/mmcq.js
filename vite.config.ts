291925e004171a6a861008ce56a41dd74bfc2811
Jerry Wang <x.jerry.wang@gmail.com>
2022-03-26 23:22:03 +0800

    chore: build with wasm

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const isBuildLib = mode === 'lib'
-
+export default defineConfig(() => {
   return {
     plugins: [react()],
     base: './',
-    publicDir: isBuildLib ? 'none' : 'public',
     build: {
-      outDir: isBuildLib ? 'dist' : 'docs',
-      lib: isBuildLib
-        ? {
-            entry: path.join(__dirname, 'src', 'lib', 'core.ts'),
-            formats: ['es', 'cjs'],
-          }
-        : false,
+      outDir: 'docs',
     },
   }
 })
